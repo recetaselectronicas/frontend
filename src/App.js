63d7d7f4d50378aef22c9ad41bd7f4126cc63195
Guusy <guusygonzalo@gmail.com>
2019-06-25 21:27:31 -0300

    agrego pagina 404

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Navbar from './components/navbar/Navbar';
 import EmitRecipe from './pages/medic/EmitRecipe';
 import PrescriptionsPage from './pages/prescriptions/PrescriptionsPage';
@@ -7,9 +7,22 @@ import PrescriptionPage from './pages/prescriptionPage/PrescriptionPage';
 import Norm from './pages/norms/norms';
 import LoginPage from './pages/loginPage/LoginPage';
 import HomePage from './pages/homePage/HomePage';
-
+import notFound from './404.png';
 import './index.css';
 
+const NoMatch = ({ location }) => (
+  <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+    <div style={{ textAlign: 'center' }}>
+      <img src={notFound} alt="" />
+      <h3>
+        La url
+        {` ${location.pathname}`}
+        {' '}
+no existe
+      </h3>
+    </div>
+  </div>
+);
 function App() {
   const [type, setType] = useState('');
   return (
@@ -18,11 +31,13 @@ function App() {
       <main>
         <Route path="/" exact render={props => <HomePage {...props} setType={setType} />} />
         <Route path="/login" exact render={props => <LoginPage {...props} type={type} />} />
-
-        <Route path="/emitir" component={EmitRecipe} />
-        <Route path="/recetas" component={PrescriptionsPage} />
-        <Route path="/receta/:id" exact component={PrescriptionPage} />
-        <Route path="/normas" exact component={Norm} />
+        <Switch>
+          <Route path="/emitir" component={EmitRecipe} />
+          <Route path="/recetas" component={PrescriptionsPage} />
+          <Route path="/receta/:id" exact component={PrescriptionPage} />
+          <Route path="/normas" exact component={Norm} />
+          <Route component={NoMatch} />
+        </Switch>
       </main>
     </Router>
   );
