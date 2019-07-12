3b5522281a15afc9b89bdac4148f109d787d617b
Leandro Devoto <leandro.devoto@mercadolibre.com>
2019-07-11 22:50:49 -0300

    Fix linter

--- src/pages/medic/EmitRecipe.js
+++ src/pages/medic/EmitRecipe.js
@@ -22,7 +22,6 @@ import AffiliateService from '../../services/AffilateService';
 import PrescriptionService from '../../services/PrescriptionService';
 import PrescriptionRequest from '../../requestBuilders/PrescriptionRequest';
 import SnackbarWrapper from '../../components/snackbarWrapper/SnackbarWrapper';
-import SessionService from '../../services/SessionService';
 
 const useStyles = makeStyles(theme => ({
   root: {
