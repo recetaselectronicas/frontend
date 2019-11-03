00ae9dc155e62a973874c059e26ced8684553a3f
Leandro Devoto <leandro.devoto@mercadolibre.com>
2019-11-03 16:24:58 -0300

    linter choto

--- src/pages/createAccount/CreateAffiliateAccount/AccountsUtils.js
+++ src/pages/createAccount/CreateAffiliateAccount/AccountsUtils.js
@@ -28,63 +28,53 @@ const getValidatedFieldDefault = (field) => {
 
 const getValidatedDateFieldDefault = (field) => {
   const newField = { ...field };
-  // eslint-disable-next-line no-restricted-globals
   newField.error = newField.required && !newField.value ? 'Campo requerido' : '';
+  // eslint-disable-next-line no-restricted-globals
   if (!newField.error) newField.error = !(newField.value instanceof Date) || isNaN(newField.value.getTime()) ? 'Fecha inválida' : '';
   return newField;
 };
 
-export const getNewFieldData = (fieldName, value = '', required = true, error = '', getValidatedField = getValidatedFieldDefault) => {
-  return { fieldName, value, required, error, getValidatedField };
-};
+export const getNewFieldData = (fieldName, value = '', required = true, error = '', getValidatedField = getValidatedFieldDefault) => ({ fieldName, value, required, error, getValidatedField });
 
-export const getEmptyAffiliateData = () => {
-  return {
-    name: getNewFieldData('name'),
-    surName: getNewFieldData('surName'),
-    userName: getNewFieldData('userName'),
-    password: getNewFieldData('password'),
-    birthDate: getNewFieldData('birthDate', null, true, '', getValidatedDateFieldDefault),
-    gender: getNewFieldData('gender'),
-    contactNumber: getNewFieldData('contactNumber'),
-    email: getNewFieldData('email'),
-    address: getNewFieldData('address'),
-    nationality: getNewFieldData('nationality'),
-    nicNumber: getNewFieldData('nicNumber'),
-    nicType: getNewFieldData('nicType'),
-    nicPhoto: getNewFieldData('nicPhoto', '', false),
-  };
-};
+export const getEmptyAffiliateData = () => ({
+  name: getNewFieldData('name'),
+  surName: getNewFieldData('surName'),
+  userName: getNewFieldData('userName'),
+  password: getNewFieldData('password'),
+  birthDate: getNewFieldData('birthDate', null, true, '', getValidatedDateFieldDefault),
+  gender: getNewFieldData('gender'),
+  contactNumber: getNewFieldData('contactNumber'),
+  email: getNewFieldData('email'),
+  address: getNewFieldData('address'),
+  nationality: getNewFieldData('nationality'),
+  nicNumber: getNewFieldData('nicNumber'),
+  nicType: getNewFieldData('nicType'),
+  nicPhoto: getNewFieldData('nicPhoto', '', false),
+});
 
-export const getAffiliateData = () => {
-  return {
-    name: getNewFieldData('name', 'Leandro'),
-    surName: getNewFieldData('surName', 'Devoto'),
-    userName: getNewFieldData('userName', 'ldevoto'),
-    password: getNewFieldData('password', '1234'),
-    birthDate: getNewFieldData('birthDate', new Date(), true, '', getValidatedDateFieldDefault),
-    gender: getNewFieldData('gender', 'M'),
-    contactNumber: getNewFieldData('contactNumber', '123123'),
-    email: getNewFieldData('email', 'ldevotox@gmail.com'),
-    address: getNewFieldData('address', 'Llavallol 3655'),
-    nationality: getNewFieldData('nationality', 'ARGENTINA'),
-    nicNumber: getNewFieldData('nicNumber', '37277876'),
-    nicType: getNewFieldData('nicType', 'DNI'),
-    nicPhoto: getNewFieldData('nicPhoto', '', false),
-  };
-};
+export const getAffiliateData = () => ({
+  name: getNewFieldData('name', 'Leandro'),
+  surName: getNewFieldData('surName', 'Devoto'),
+  userName: getNewFieldData('userName', 'ldevoto'),
+  password: getNewFieldData('password', '1234'),
+  birthDate: getNewFieldData('birthDate', new Date(), true, '', getValidatedDateFieldDefault),
+  gender: getNewFieldData('gender', 'M'),
+  contactNumber: getNewFieldData('contactNumber', '123123'),
+  email: getNewFieldData('email', 'ldevotox@gmail.com'),
+  address: getNewFieldData('address', 'Llavallol 3655'),
+  nationality: getNewFieldData('nationality', 'ARGENTINA'),
+  nicNumber: getNewFieldData('nicNumber', '37277876'),
+  nicType: getNewFieldData('nicType', 'DNI'),
+  nicPhoto: getNewFieldData('nicPhoto', '', false),
+});
 
-export const hasError = (accountData) => {
-  return Object.keys(accountData).some(key => accountData[key].error);
-};
+export const hasError = accountData => Object.keys(accountData).some(key => accountData[key].error);
 
-export const calculateErrors = (accountData) => {
-  return Object.keys(accountData).reduce((newAccountData, key) => {
-    // eslint-disable-next-line no-param-reassign
-    newAccountData[key] = accountData[key].getValidatedField(accountData[key]);
-    return newAccountData;
-  }, {});
-};
+export const calculateErrors = accountData => Object.keys(accountData).reduce((newAccountData, key) => {
+  // eslint-disable-next-line no-param-reassign
+  newAccountData[key] = accountData[key].getValidatedField(accountData[key]);
+  return newAccountData;
+}, {});
 
 export const getAffiliatePayload = (accountData) => {
   const { name, surName, userName, password, birthDate, gender, contactNumber, email, address, nationality, nicNumber, nicType, nicPhoto } = accountData;
@@ -107,7 +97,7 @@ export const getAffiliatePayload = (accountData) => {
 
 export const parseAffiliateResponseError = (accountData, response) => {
   const newAccountData = { ...accountData };
-  if (response.type !== 'UNIFY') return false
+  if (response.type !== 'UNIFY') return false;
   if (response.code === '1-000') {
     if (response.cause.field === newAccountData.userName.fieldName) {
       newAccountData.userName.error = 'Usuario en uso';
