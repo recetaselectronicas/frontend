7b3b37c0018d03b4720a445a3e92ecfee2713457
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-11-18 14:51:14 -0300

    Now the medical insurance page can see linked OS, and unlik this (only for doctors)

--- src/services/MedicalInsuranceService.js
+++ src/services/MedicalInsuranceService.js
@@ -10,4 +10,8 @@ export default class MedicalInsuranceService extends RestclientService {
     // return new Promise(res => res([{ id: 0, label: 'OSDE' }, { id: 1, label: 'Swiss medical' }, { id: 2, label: 'ACA Salud' }]));
     return this.get('/doctors/medical-insurances');
   }
+
+  static getLinkedMedicalInsurances() {
+    return this.get('/session/medical-insurances');
+  }
 }
