85b6b96a0643900205049abfef1365b5d4c2b266
Gonzalo Gras Cantou <guusygonzalo@gmail.com>
2019-11-20 01:05:21 -0300

    Now can linkup in doctor page, and search by nicNumber and nationalMatriculation

--- src/pages/doctors/DoctorsPage.js
+++ src/pages/doctors/DoctorsPage.js
@@ -1,13 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from '@material-ui/core';
+import { Container, Paper, Button, Typography, FormControl, TextField } from '@material-ui/core';
 import MedicalInsuranceService from '../../services/MedicalInsuranceService';
 import LinksService from '../../services/LinksService';
 import withSnackbar from '../../components/hocs/withSnackbar';
 import { UsersList } from '../../components/usersList/UsersList';
+import FindByNicNumber from '../../components/findByNicNumber/FindByNicNumber';
+import DoctorService from '../../services/DoctorService';
+
+
+const nicNumberFlow = "nicNumber"
+const nationalMatriculationFlow = "nationalMatriculation"
 
 const DoctorsPage = ({ showSuccess, showError }) => {
     const [doctors, setDoctors] = useState([]);
-
+    const [searchDoctors, setSearchDoctors] = useState([]);
+    const [selectedFlow, setSelectedFlow] = useState(null);
+    const [nicNumber, setNicnumber] = useState('');
+    const [gender, setGender] = useState('');
+    const [nationalMatriculation, setNationalMatriculation] = useState('');
     async function fetchData() {
         try {
             const data = await MedicalInsuranceService.getDoctors();
@@ -21,6 +31,14 @@ const DoctorsPage = ({ showSuccess, showError }) => {
         fetchData();
     }, []);
 
+    const cleanInputs = () => {
+        setNicnumber('')
+        setGender('')
+        setNationalMatriculation('')
+        setSelectedFlow(null)
+        setSearchDoctors([])
+    }
+
     const handleError = (error) => {
         if (error.message) {
             showError(error.message);
@@ -33,19 +51,80 @@ const DoctorsPage = ({ showSuccess, showError }) => {
         try {
             await LinksService.unlink({
                 doctor: {
+                    id: doctor.id,
+                },
+            });
+            showSuccess('Se desvinculo con exito el doctor !');
+            await fetchData();
+        } catch (error) {
+            handleError(error);
+        }
+    };
+
+    const search = async () => {
+        try {
+            const doctorsData = await DoctorService.search({ gender, nicNumber, nationalMatriculation })
+            setSearchDoctors(doctorsData)
+        } catch (error) {
+            handleError(error)
+        }
+    }
+
+    const linkup = async (doctor) => {
+        try {
+            await LinksService.requestLink({
+                doctor: {
                     id: doctor.id
                 }
             })
-            showSuccess('Se desvinculo con exito el doctor !')
+            showSuccess('Se vinculo con exito el doctor !');
+            cleanInputs();
             await fetchData();
         } catch (error) {
             handleError(error)
         }
-    };
+    }
+
+    const selectFlow = (flow) => {
+        setSelectedFlow(flow)
+        setSearchDoctors([])
+    }
 
     return (
         <Container>
-            <UsersList users={doctors} unlink={unlink} emptyState='No tienes doctores asociados' />
+            <Paper style={{ padding: '2em', marginBottom: '2em' }}>
+                <Typography variant="h5">Agregar un medico</Typography>
+                {!selectedFlow
+                    && (
+                        <>
+                            <Button onClick={() => selectFlow(nicNumberFlow)}>Por DNI</Button>
+                            <Button onClick={() => selectFlow(nationalMatriculationFlow)}>Por matricula nacional</Button>
+                        </>
+                    )}
+                {selectedFlow === nicNumberFlow && (
+                    <FindByNicNumber
+                        gender={gender}
+                        nicNumber={nicNumber}
+                        setNicnumber={setNicnumber}
+                        setGender={setGender}
+                        search={search}
+                    />
+                )}
+
+                {selectedFlow === nationalMatriculationFlow && (
+                    <FormControl>
+                        <TextField label="Matricula nacional" value={nationalMatriculation} onChange={event => setNationalMatriculation(event.target.value)} />
+                        <Button onClick={search}>Buscar</Button>
+                    </FormControl>
+                )}
+
+                {searchDoctors.length > 0 && <UsersList users={searchDoctors} onClick={linkup} labelButton="Vincular" />}
+
+
+
+
+            </Paper>
+            <UsersList users={doctors} onClick={unlink} emptyState="No tienes doctores asociados" labelButton="Desvincular" />
         </Container>
     );
 };
