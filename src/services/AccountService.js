d6240c286e9676b79811f57c17b83c5cf56da221
Leandro Devoto <leandro.devoto@mercadolibre.com>
2019-12-17 18:55:00 -0300

    doubling create account timeout

--- src/services/AccountService.js
+++ src/services/AccountService.js
@@ -3,15 +3,15 @@ import RestclientService from './RestclientService';
 export default class AccountService extends RestclientService {
 
   static createAffiliateAccount(data) {
-    return this.post('/users/affiliate', data, { timeout: 5000 });
+    return this.post('/users/affiliate', data, { timeout: 10000 });
   }
 
   static createDoctorAccount(data) {
-    return this.post('/users/doctor', data, { timeout: 5000 });
+    return this.post('/users/doctor', data, { timeout: 10000 });
   }
 
   static createPharmacistAccount(data) {
-    return this.post('/users/pharmacist', data, { timeout: 5000 });
+    return this.post('/users/pharmacist', data, { timeout: 10000 });
   }
 
   static verifyAccount(userType, token) {
